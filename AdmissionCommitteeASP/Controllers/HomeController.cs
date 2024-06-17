15e1f28d30adda750775cdc499dab3c06c47cf62
prtsie <yonhsup@mail.ru>
2024-06-17 19:20:32 +0300

    Initial commit

---
+++ AdmissionCommitteeASP/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using AdmissionCommitteeASP.Models;
+
+namespace AdmissionCommitteeASP.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        this.logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
